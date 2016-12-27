321d162ae202b93643169f5a4434bea2449a53d4
Lobo Chu <Lobo.Chu@audatex.com>
2016-12-27 11:23:00 +0800

    Improved formatting

--- src/test/java/tw/lobo/trivia/test/GameRunnerGolderMasterTest.java
+++ src/test/java/tw/lobo/trivia/test/GameRunnerGolderMasterTest.java
@@ -1,37 +1,32 @@
-
 package tw.lobo.trivia.test;
+
 import com.adaptionsoft.games.uglytrivia.Game;
 
 import java.util.Random;
 
-
 //Intentionally duplicates producton GameRunner for now.
 public class GameRunnerGolderMasterTest {
 
-	private static boolean notAWinner;
+  private static boolean notAWinner;
 
-	public static void main(String[] args) {
-		Game aGame = new Game();
-		
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
-		
-		Random rand = new Random(7777);
-	
-		do {
-			
-			aGame.roll(rand.nextInt(5) + 1);
-			
-			if (rand.nextInt(9) == 7) {
-				notAWinner = aGame.wrongAnswer();
-			} else {
-				notAWinner = aGame.wasCorrectlyAnswered();
-			}
-			
-			
-			
-		} while (notAWinner);
-		
-	}
+  public static void main(String[] args) {
+    Game aGame = new Game();
+
+    aGame.add("Chet");
+    aGame.add("Pat");
+    aGame.add("Sue");
+
+    Random rand = new Random(7777);
+
+    do {
+      aGame.roll(rand.nextInt(5) + 1);
+
+      if (rand.nextInt(9) == 7) {
+        notAWinner = aGame.wrongAnswer();
+      } else {
+        notAWinner = aGame.wasCorrectlyAnswered();
+      }
+    } while (notAWinner);
+
+  }
 }
