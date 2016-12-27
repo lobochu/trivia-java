39f5e10e1a350ad07bc2ee3ff65c586d0f97c56b
Lobo Chu <Lobo.Chu@audatex.com>
2016-12-27 11:09:46 +0800

    Adjust formatting

--- src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -1,5 +1,5 @@
-
 package com.adaptionsoft.games.trivia.runner;
+
 import com.adaptionsoft.games.uglytrivia.Game;
 
 import java.util.Random;
@@ -9,30 +9,28 @@ import java.util.Random;
 // so why is it in GameRunner?
 public class GameRunner {
 
-	private static boolean notAWinner;
+  private static boolean notAWinner;
 
-	public static void main(String[] args) {
-		Game aGame = new Game();
-		
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
-		
-		Random rand = new Random();
-	
-		do {
-			
-			aGame.roll(rand.nextInt(5) + 1);
-			
-			if (rand.nextInt(9) == 7) {
-				notAWinner = aGame.wrongAnswer();
-			} else {
-				notAWinner = aGame.wasCorrectlyAnswered();
-			}
-			
-			
-			
-		} while (notAWinner);
-		
-	}
+  public static void main(String[] args) {
+    Game aGame = new Game();
+
+    aGame.add("Chet");
+    aGame.add("Pat");
+    aGame.add("Sue");
+
+    Random rand = new Random();
+
+    do {
+
+      aGame.roll(rand.nextInt(5) + 1);
+
+      if (rand.nextInt(9) == 7) {
+        notAWinner = aGame.wrongAnswer();
+      } else {
+        notAWinner = aGame.wasCorrectlyAnswered();
+      }
+
+    } while (notAWinner);
+
+  }
 }
