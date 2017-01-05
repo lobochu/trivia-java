8d64c82fc498c9213296ebe4a18d2e091b3f42a9
Lobo Chu <Lobo.Chu@audatex.com>
2017-01-05 11:52:09 +0800

    We now have a place to generate multiple test run files

--- src/test/java/tw/lobo/trivia/test/GameRunnerGolderMasterTest.java
+++ src/test/java/tw/lobo/trivia/test/GameRunnerGolderMasterTest.java
@@ -16,35 +16,32 @@ public class GameRunnerGolderMasterTest {
 
     final int gameId = 7777;
 
-    //REFACTOR Make the current working directory more explicit?
-    final File testRunOutputFile = new File("test-data", String.format("test-run-%d.txt", gameId));
-//    PrintWriter canvas = new PrintWriter(new FileWriter(testRunOutputFile));
-//    canvas.println("Hello, world!");
-//    canvas.flush();
-//    canvas.close();
+    for (int i = 0; i<1 ; i++) {
+      //REFACTOR Make the current working directory more explicit?
+      final File testRunOutputFile = new File("test-data", String.format("test-run-%d.txt", gameId));
 
+      // REFACTOR Make the game output stream pluggable, since clearly, I want to plug
+      // something else into it.
+      System.setOut(new PrintStream(testRunOutputFile));
+      Game aGame = new Game();
 
-    // REFACTOR Make the game output stream pluggable, since clearly, I want to plug
-    // something else into it.
-    System.setOut(new PrintStream(testRunOutputFile));
-    Game aGame = new Game();
+      aGame.add("Chet");
+      aGame.add("Pat");
+      aGame.add("Sue");
 
-    aGame.add("Chet");
-    aGame.add("Pat");
-    aGame.add("Sue");
+      // We can conveniently use the game ID directly as a random number generator seed.
+      Random rand = new Random(gameId);
 
-    // We can conveniently use the game ID directly as a random number generator seed.
-    Random rand = new Random(gameId);
+      do {
+        aGame.roll(rand.nextInt(5) + 1);
 
-    do {
-      aGame.roll(rand.nextInt(5) + 1);
-
-      if (rand.nextInt(9) == 7) {
-        notAWinner = aGame.wrongAnswer();
-      } else {
-        notAWinner = aGame.wasCorrectlyAnswered();
-      }
-    } while (notAWinner);
+        if (rand.nextInt(9) == 7) {
+          notAWinner = aGame.wrongAnswer();
+        } else {
+          notAWinner = aGame.wasCorrectlyAnswered();
+        }
+      } while (notAWinner);
+    }
 
     System.setOut(systemOut);
 
